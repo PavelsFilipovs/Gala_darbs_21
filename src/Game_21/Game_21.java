cffff797bb4763a860afbcf9cfe995d550689bb9
Pāvels Fiļipovs <pavels.filipovs.oop@gmail.com>
2021-12-18 03:00:37 +0200

    Insert basic

---
+++ src/Game_21/Game_21.java
@@ -0,0 +1,8 @@
+package Game_21;
+
+public class Game_21 {
+	public Game_21() {
+	
+		System.out.println("Lets go!");
+	}
+}
---
+++ src/Game_21/Main.java
@@ -0,0 +1,10 @@
+package Game_21;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Game_21 game_21 = new Game_21();
+
+	}
+
+}
