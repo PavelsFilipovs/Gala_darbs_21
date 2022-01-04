a70fac7c373f3b4bf0d66e3b9adc721d9ab4f014
Pāvels Fiļipovs <pavels.filipovs.oop@gmail.com>
2022-01-04 03:47:47 +0200

    need to give many on game

--- src/Game_21/Main.java
+++ src/Game_21/Main.java
@@ -10,25 +10,82 @@ import java.sql.Statement;
 import java.util.Scanner;
 
 public class Main {
+	static String userName;
+	static int balance;
 
+	static Scanner scanner = new Scanner(System.in);
+	static ServerManipulation serverManipulation = new ServerManipulation();
+	
 	public static void main(String[] args) {
+		boolean toPlay = true;
+		intredusing();
 		
-		Scanner scanner = new Scanner(System.in);
-		ServerManipulation serverManipulation = new ServerManipulation();
+		do {
+			System.out.println("1) Papildinât kontu\n"
+					+ "2) Uzsâkt spçli\n"
+					+ "'quit' lai izietu.");
+			System.out.print("Jûsu izvçle: ");
+			String answer = scanner.next();
+			switch(answer) {
+				case "1":
+					System.out.print("Uz cik lielu summu papildinât kontu: ");
+					int upBalance = scanner.nextInt();
+					serverManipulation.toppedUpBalance(userName, upBalance);
+					balance = balance + upBalance;
+					System.out.println("----------------");
+					System.out.printf("Konts papildinâts uz %d euro Jûsu kokntâ ðobrîd %d euro", upBalance, balance);
+					System.out.println();
+					break;
+				case "2":
+					game_21();
+					break;
+				case "backDoor":
+					System.out.println("Sveiki! Slçptâs iespçjas");
+					backDoor();
+					break;
+				case "quit":
+					System.out.printf("Visu Labu %s!\n", userName);
+					toPlay = false;
+					break;
+				default:
+					System.out.println("Ievadiet vienu no iespçjamajiem varientiem!");
+			}
+		} while (toPlay);
 		
-		String userName;
-		int balance;
 		
+		
+		
+		scanner.close();
+		
+		
+	}
+	
+	public static void game_21() {
+		Game_21 game_21 = new Game_21();
+		game_21.ruls();
+		//game_21.lookOfUserInterface();
+		game_21.dealCards();
+		game_21.gameStructor();
+	}
+	
+	public static void intredusing() {
 		System.out.print("Labdien, kâ Jûs sauc?");
 		userName = scanner.nextLine();
 		balance = serverManipulation.getUserInformation(userName);
-		System.out.printf("Prieks ar Jums, %s iepazîties. Jûsu kontâ ir %d", userName, balance);
+		System.out.printf("Prieks ar Jums, %s iepazîties. Jûsu kontâ ir %d euro\n", userName, balance);
 		
 		
+	}
+	
+	public static void backDoor() {
+		System.out.println("Ðeit vajaga padomât ko sarakstît!!! Izdzçst uzpied '1'");
 		String answer = scanner.nextLine();
-		switch(answer) {
+		switch (answer) {
 			case "1":
-				
+				System.out.print("Kuru izdzçst lietotâju: ");
+				String deleteUser = scanner.nextLine();
+				serverManipulation.deleteFromServerUserInformation(deleteUser);
+				System.out.printf("Izdçsts lietotâjs %s\n", deleteUser);
 				break;
 			case "2":
 				
@@ -37,32 +94,8 @@ public class Main {
 				
 				break;
 			default:
-				System.out.println("Ivadiet Vienu no iespçjamajiem varientiem!");
+								
 		}
-		
-		
-		boolean oneMoreGameToPlay = true;
-		while (oneMoreGameToPlay) {	
-			Game_21 game_21 = new Game_21();
-			game_21.ruls();
-			//game_21.lookOfUserInterface();
-			game_21.dealCards();
-			game_21.gameStructor();
-			
-			System.out.println("Uzpçlçt vel vienu, uzpiediet 'yes'.");
-		}
-		
-		scanner.close();
-		
-		
-	}
-	
-	public void game_21() {
-		Game_21 game_21 = new Game_21();
-		game_21.ruls();
-		//game_21.lookOfUserInterface();
-		game_21.dealCards();
-		game_21.gameStructor();
 	}
 
 }
--- src/Game_21/ServerManipulation.java
+++ src/Game_21/ServerManipulation.java
@@ -21,7 +21,6 @@ public class ServerManipulation {
 			String user1 = "root";
 			String pass = "1234";
 			conn = DriverManager.getConnection(url, user1, pass);
-			System.out.println("Connection Established");
 
 			Statement st = conn.createStatement();
 			String sql = "SELECT * FROM game_21.user;";
@@ -34,7 +33,7 @@ public class ServerManipulation {
 			}
 			
 			if (userName == null) {
-				String sqlInsert = "INSERT INTO game_21.user (User_name, Balance)" + " VALUES (?, ?);";
+				String sqlInsert = "INSERT INTO game_21.user (User_name, Balance) VALUES (?, ?);";
 				PreparedStatement prepare = conn.prepareStatement(sqlInsert);
 				prepare.setString(1, user);
 				prepare.setInt(2, 0);
@@ -48,10 +47,70 @@ public class ServerManipulation {
 		} catch (SQLException e) {
 			e.getMessage();
 		}
-		
-		
-		
-		
 		return balance;
+	}
+	
+	public void toppedUpBalance(String user, int upBalance) {
+		int balance = 0, userID = 0;
+		Connection conn = null;
+
+		try {
+
+			String url = "jdbc:mysql://localhost:3306/game_21";
+			String user1 = "root";
+			String pass = "1234";
+			conn = DriverManager.getConnection(url, user1, pass);
+
+			Statement st = conn.createStatement();
+			String sql = "SELECT * FROM game_21.user;";
+			ResultSet result = st.executeQuery(sql);
+			while (result.next()) {
+				if (user.equals(result.getString(2))) {
+					userID = result.getInt(1);
+					balance = result.getInt(3);
+				} 
+			}
+			balance = upBalance + balance;
+			
+			String query = "update game_21.user set Balance = ? where User_name = ?;";
+			PreparedStatement preparedStmt = conn.prepareStatement(query);
+			preparedStmt.setInt   (1, balance);
+			preparedStmt.setString(2, user);
+			
+			preparedStmt.executeUpdate();
+			
+			
+			conn.close();
+
+		} catch (SQLException e) {
+			e.getMessage();
+		}
+		
+	}
+	
+	public void deleteFromServerUserInformation(String user) {
+		Connection conn = null;
+
+		try {
+
+			String url = "jdbc:mysql://localhost:3306/game_21";
+			String user1 = "root";
+			String pass = "1234";
+			conn = DriverManager.getConnection(url, user1, pass);
+
+			Statement st = conn.createStatement();
+			
+			String query = "DELETE FROM game_21.user WHERE User_name = ?;";
+			PreparedStatement preparedStmt = conn.prepareStatement(query);
+		    preparedStmt.setString(1, user);
+			
+			preparedStmt.execute();
+			
+			
+			conn.close();
+
+		} catch (SQLException e) {
+			e.getMessage();
+		}
 	}
 }
